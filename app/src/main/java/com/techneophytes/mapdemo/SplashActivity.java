23223811e642240fed64f2ad7be308ca4c3d24c3
Deependra Singh Rajput <techdsr1505@gmail.com>
2019-02-23 19:28:04 +0530

    splash screen

---
+++ app/src/main/java/com/techneophytes/mapdemo/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.techneophytes.mapdemo;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(SplashActivity.this, HospitalActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
